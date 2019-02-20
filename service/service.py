9185c0f61c001a92d2302c0b2b7f3edced5ea1bf
Mike Trout <mike.trout@gmail.com>
2019-02-20 11:35:02 +0000

    Check service output exists before trying to delete.

--- service/service.py
+++ service/service.py
@@ -5,31 +5,34 @@ import subprocess
 from flask import Flask
 
 app = Flask(__name__)
+serviceOutput = "/tmp/service.out"
 
 
 @app.route("/", methods=["GET"])
 def get():
-    os.remove("/tmp/service.out")
+    if os.path.exists(serviceOutput):
+        os.remove(serviceOutput)
     p = subprocess.Popen("natural madio=0 batchmode cmsynin=/service/get.cmd cmobjin=/service/get.cmd cmprint=/tmp/out natlog=err",
                          shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in p.stdout.readlines():
         print(line)
     retval = p.wait()
     print(retval)
-    f = os.open("/tmp/service.out", "r")
+    f = os.open(serviceOutput, "r")
     return f.read()
 
 
 @app.route("/", methods=["POST"])
 def post():
-    os.remove("/tmp/service.out")
+    if os.path.exists(serviceOutput):
+        os.remove(serviceOutput)
     p = subprocess.Popen("natural madio=0 batchmode cmsynin=/service/post.cmd cmobjin=/service/post.cmd cmprint=/tmp/out natlog=err",
                          shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in p.stdout.readlines():
         print(line)
     retval = p.wait()
     print(retval)
-    f = os.open("/tmp/service.out", "r")
+    f = os.open(serviceOutput, "r")
     return f.read()
 
 
