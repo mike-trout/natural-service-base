8b8b688934f1368cb537d2509329de16aebed28f
Mike Trout <mike.trout@gmail.com>
2019-02-20 11:19:58 +0000

    Remove service output before calling. Set MADIO to 0.

--- service/service.py
+++ service/service.py
@@ -9,25 +9,27 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def get():
-    p = subprocess.Popen("natural batchmode cmsynin=/service/get.cmd cmobjin=/service/get.cmd cmprint=/tmp/out natlog=err",
+    os.remove("/tmp/service.out")
+    p = subprocess.Popen("natural madio=0 batchmode cmsynin=/service/get.cmd cmobjin=/service/get.cmd cmprint=/tmp/out natlog=err",
                          shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in p.stdout.readlines():
         print(line)
     retval = p.wait()
     print(retval)
-    f = open("/tmp/service.out", "r")
+    f = os.open("/tmp/service.out", "r")
     return f.read()
 
 
 @app.route("/", methods=["POST"])
 def post():
-    p = subprocess.Popen("natural batchmode cmsynin=/service/post.cmd cmobjin=/service/post.cmd cmprint=/tmp/out natlog=err",
+    os.remove("/tmp/service.out")
+    p = subprocess.Popen("natural madio=0 batchmode cmsynin=/service/post.cmd cmobjin=/service/post.cmd cmprint=/tmp/out natlog=err",
                          shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in p.stdout.readlines():
         print(line)
     retval = p.wait()
     print(retval)
-    f = open("/tmp/service.out", "r")
+    f = os.open("/tmp/service.out", "r")
     return f.read()
 
 
